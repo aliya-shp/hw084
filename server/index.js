baa37d7d0886a2f91bfb28369d81f8f8e4a4218b
Aliya Akmatova <amavarati@gmail.com>
2020-03-08 02:28:17 +0600

    Added users instance

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+const users = require('./app/users');
+
 const app = express();
 const port = 8000;
 
@@ -10,17 +12,13 @@ app.use(cors());
 app.use(express.static('public'));
 
 const run = async () => {
-    await mongoose.connect('mongodb://localhost/music', {
+    await mongoose.connect('mongodb://localhost/todo-list', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
     });
 
-    // app.use('/artists', artists);
-    // app.use('/albums', albums);
-    // app.use('/tracks', tracks);
-    // app.use('/users', users);
-    // app.use('/track_histories', trackHistories);
+    app.use('/users', users);
 
     app.listen(port, () => {
         console.log(`HTTP Server started on ${port} port!`);
